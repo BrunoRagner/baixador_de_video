6f10457f8b595005b6fd486ddfa7628102aa3c09
Bruno Ragner <85395784+BrunoRagner@users.noreply.github.com>
2022-03-24 16:31:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 
-
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
+yt = yt
+yt = YouTube('')
 yt.title()
 yt.thumbnail_url()
 stream = yt.streams.get_by_itag(22)
